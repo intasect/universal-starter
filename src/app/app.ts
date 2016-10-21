bf15ee86038c3e56440ed168602468f4d4a64d00
ZhihuiTian <tian.zhihui@intasect.co.jp>
2016-10-21 10:11:01 +0900

    change something.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, AfterContentInit, Renderer } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app',
@@ -13,8 +14,20 @@ export class App {
 
 @Component({
   selector: 'home',
-  template: 'Home component'
+  template: 'Home component {{test}} <div id="test"></div>'
 })
-export class Home {
+export class Home implements AfterContentInit{
+  test: string = '123';
+  constructor(private e: ElementRef, private render: Renderer, private http: Http) {
+    this.http.get('/data.json').subscribe(data => {
+      this.test = data.text();
+    });
+  }
 
+  ngAfterContentInit() {
+    let d = this.render.createElement(this.e.nativeElement, 'script');
+    this.render.setElementAttribute(d, 'src', 'http://code.jquery.com/jquery-3.1.1.js');
+    let d1 = this.render.createElement(this.e.nativeElement, 'script');
+    this.render.createText(d1, '$("#test").html("1234")');
+  }
 }
